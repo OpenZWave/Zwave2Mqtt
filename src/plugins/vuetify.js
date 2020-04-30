03a2e68e3dc32e2042c6c6514ec4d252e5cc19a3
Daniel Lando <daniel.sorridi@gmail.com>
2020-04-30 16:37:23 +0200

    WIP: moved to webpack v4

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+// src/plugins/vuetify.js
+
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
+const opts = {}
+
+export default new Vuetify(opts)
