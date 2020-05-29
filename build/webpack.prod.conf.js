ae5564a86bd48122a2ad7b4e503c05c48f7313f0
Fabio Torchetti <fabio.torchetti@accenture.com>
2020-05-29 17:34:29 -0500

    Use EJS for index page in dev.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -52,21 +52,6 @@ const webpackConfig = merge(baseWebpackConfig, {
         ? { safe: true, map: { inline: false } }
         : { safe: true }
     }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: config.build.index,
-      template: 'index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-    }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
--- config/app.js
+++ config/app.js
@@ -1,5 +1,7 @@
 // config/app.js
 module.exports = {
+	'title': 'ZWave to MQTT',
 	'storeDir': 'store',
+	'base': '/some/zwave',
 	'port': 8091
 };
