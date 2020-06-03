6b1a593fd980aa70649b792f63f36df617f0be92
Fabio Torchetti <fabio.torchetti@accenture.com>
2020-06-03 14:08:00 -0500

    Proxy to nodejs service for development of UI

--- config/index.js
+++ config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/socket.io': {
+        target: 'ws://localhost:8091',
+        ws: true
+     },
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
